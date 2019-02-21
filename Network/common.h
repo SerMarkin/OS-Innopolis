78e219127a0851920afccb1f67880f1361f13c7c
Sergey <kadil12353@gmail.com>
2019-02-21 12:34:06 +0300

    Add files via upload

---
+++ Network/common.h
@@ -0,0 +1,16 @@
+#include <string.h>
+
+
+typedef struct _test_struct{
+    
+    char a[50];
+    char b[50];
+    unsigned int c;
+} test_struct_t;
+
+
+typedef struct result_struct_{
+
+     char c[150];
+
+} result_struct_t;
